f35b82b8046377d5170e13cb24cc7e54f97763e8
Dandy Nagel <94107254+GittyX3b@users.noreply.github.com>
2025-12-05 12:54:16 +0100

    setup frontend with router, daisyui and tailwind (#2)

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>pages/Home.tsx</div>;
+};
+
+export { Home };
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { fileURLToPath } from "url";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "/", // set github reponame for deploy
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+      },
+    },
+    outDir: "dist",
+    emptyOutDir: true,
+    cssCodeSplit: false,
+    cssMinify: true,
+    minify: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@layouts": path.resolve(__dirname, "./src/layouts"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+    },
+  },
+  plugins: [react(), tailwindcss()],
+});
