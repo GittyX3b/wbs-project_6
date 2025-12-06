a20654b0365954e707c5d5025a3a2261aa034fa1
Alissa <alissa.samara@my.mail.de>
2025-12-06 20:21:26 +0100

    First draft of Sign up form created

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,9 +1,40 @@
-import { useState, type ReactElement } from "react";
+import { useState, useEffect } from "react";
 
-export function SignUp(): ReactElement {
+type Direction = "init" | "success" | "badRequest" | "userExist";
+
+function submitSignUp(event) {
+  event.preventDefault();
+  console.log("Sign Up Submit Event");
+}
+
+export function SignUp() {
+  const [signUpState, setSignUpState] = useState((): Direction => "init");
   return (
     <>
-      <div className="flex flex-col items-center justify-center">SignUp</div>
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="font-bold text-2xl m-4">Sign Up</h1>
+        <form className="flex flex-col" onSubmit={submitSignUp}>
+          <label htmlFor="email">E-Mail: </label>
+          <input type="email" name="email" id="email" />
+          <label htmlFor="password">Password: </label>
+          <input type="password" name="password" id="password" />
+          <button
+            className=" rounded-xl bg-indigo-700 hover:bg-fuchsia-700"
+            type="submit"
+          >
+            Register
+          </button>
+        </form>
+        <p className="text-red-600 mt-2">
+          {signUpState === "userExist"
+            ? "🛑 User already exists!"
+            : signUpState === "badRequest"
+              ? "🛑 Password too short! At least 8 characters!"
+              : signUpState === "success"
+                ? "Sign up successful!"
+                : "♥️ Please fill out form for sign up ♥️"}
+        </p>
+      </div>
     </>
   );
 }
--- frontend/src/utils/apiAccess.ts
+++ frontend/src/utils/apiAccess.ts
@@ -1,3 +1,16 @@
 export function createUser(email: string, password: string): object {
   return { email: email, password: password };
 }
+
+const options = {
+  method: "POST",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${accessTokenAuth}`,
+  },
+};
+
+function regexCheckEmail(input: string): boolean {
+  const regex = /@.*\./i;
+  return regex.test(input);
+}
